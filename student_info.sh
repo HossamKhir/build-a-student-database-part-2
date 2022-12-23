c2ba98bed8c90c562b83edb615cc970d4230b100
HossamKhir <h.khair95@outlook.com>
2022-12-23 17:15:20 +0000

    Save progress: 1960.1

--- student_info.sh
+++ student_info.sh
@@ -67,3 +67,5 @@ QUERY="
 SELECT ROUND(AVG(gpa), 2)
 FROM students;";
 echo "$($PSQL "$QUERY")";
+
+echo -e "\nMajor ID, total number of students in a column named 'number_of_students', and average GPA rounded to two decimal places in a column name 'average_gpa', for each major ID in the students table having a student count greater than 1:";
