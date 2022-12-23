c014d58b986739ace6b5ee27fd4fb4a06e4c1155
HossamKhir <h.khair95@outlook.com>
2022-12-23 16:36:47 +0000

    Save progress: 1780.1

--- student_info.sh
+++ student_info.sh
@@ -6,48 +6,37 @@ PSQL="psql -X --username=freecodecamp --dbname=students --no-align --tuples-only
 
 echo -e "\nFirst name, last name, and GPA of students with a 4.0 GPA:"
 QUERY="
-SELECT
-  first_name, last_name, gpa
-FROM
-  students
-WHERE
-  gpa = 4.0;"
+SELECT first_name,
+    last_name,
+    gpa
+FROM students
+WHERE gpa = 4.0;"
 echo "$($PSQL "$QUERY")"
 
 echo -e "\nAll course names whose first letter is before 'D' in the alphabet:"
 QUERY="
-SELECT
-  course
-FROM
-  courses
-WHERE
-  course < 'D';"
+SELECT course
+FROM courses
+WHERE course < 'D';"
 echo "$($PSQL "$QUERY")";
 
 echo -e "\nFirst name, last name, and GPA of students whose last name begins with an 'R' or after and have a GPA greater than 3.8 or less than 2.0:";
 QUERY="
-SELECT
-  first_name, last_name, gpa
-FROM
-  students
-WHERE
-  last_name >= 'R'
-AND
-  gpa > 3.8
-  OR
-  gpa < 2.0;"
+SELECT first_name,
+    last_name,
+    gpa
+FROM students
+WHERE last_name >= 'R'
+    AND gpa > 3.8
+    OR gpa < 2.0;"
 echo "$($PSQL "$QUERY")";
 
 echo -e "\nLast name of students whose last name contains a case insensitive 'sa' or have an 'r' as the second to last letter:";
 QUERY="
-SELECT
-  last_name
-FROM
-  students
-WHERE
-  last_name ILIKE '%sa%'
-OR
-  last_name LIKE '%r_';";
+SELECT last_name
+FROM students
+WHERE last_name ILIKE '%sa%'
+    OR last_name LIKE '%r_';";
 echo "$($PSQL "$QUERY")";
 
 echo -e "\nFirst name, last name, and GPA of students who have not selected a major and either their first name begins with 'D' or they have a GPA greater than 3.0:";
